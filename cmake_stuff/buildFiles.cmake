6418f6afab54ce025553b5f3ecad00c95b44a1d3
jmgonzalez <josgonmar14@gmail.com>
2023-11-23 11:53:39 +0100

    Cleaner CMakeLists.txt

--- cmake_stuff/buildFiles.cmake
+++
@@ -1,6 +0,0 @@
-
-# ----------------------------------------------------------------------------
-#   Create config.h file
-# ----------------------------------------------------------------------------
-CONFIGURE_FILE("${PROJECT_SOURCE_DIR}/cmake_stuff/config.h.in"
-   "${PROJECT_BINARY_DIR}/config.h" @ONLY)
